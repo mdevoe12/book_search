bca1ffd8ec3d1155d8c54304aa97eb229f349b60
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 15:03:03 -0700

    refactored block of index code into results method

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,20 +3,9 @@ class Api::V1::SearchController < ApplicationController
     return invalid_search if search_params.empty?
 
     response = OpenLibraryService.get(search_params)
-
     return api_error if response.status != 200
 
-    is_search_unique = Search.is_search_unique?(search_params)
-
-    parsed_body = JSON.parse(response.body)
-    books_data = BooksPresenter.format_data(parsed_body, sort_params)
-
-    result = {
-      is_search_unique: is_search_unique,
-      books: books_data
-    }
-
-    render json: result, status: 200
+    render json: results(response), status: 200
   end
 
   private
@@ -29,6 +18,17 @@ class Api::V1::SearchController < ApplicationController
     params.permit(:sort_by, :order_by)
   end
 
+  def results(response)
+    is_search_unique = Search.is_search_unique?(search_params)
+
+    parsed_body = JSON.parse(response.body)
+    books_data = BooksPresenter.format_data(parsed_body, sort_params)
+
+    {
+      is_search_unique: is_search_unique,
+      books: books_data
+    }
+  end
 
   def invalid_search
     error = {
