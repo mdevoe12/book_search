f93b5338fd361c872c5f9ac9aa29212a4d8aa17f
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 12:36:54 -0700

    first pass at finding search terms

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,9 @@
 class Search < ApplicationRecord
   belongs_to :title, optional: true
   belongs_to :author, optional: true
+
+  def self.is_search_unique?(params)
+    # Search.where(title.name: params[:title], author.name: params[:author])
+    Search.joins(:title).where(titles: { name: params[:title] }).empty?
+  end
 end
