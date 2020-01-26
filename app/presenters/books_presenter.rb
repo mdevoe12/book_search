ca978ffa47ad2872437fddc2f3fc0d8dfd107f85
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 14:02:05 -0700

    created new method that instantiates instance of class

--- app/presenters/books_presenter.rb
+++ app/presenters/books_presenter.rb
@@ -1,8 +1,16 @@
 class BooksPresenter
-  attr_reader :raw_info
+  attr_reader :raw_info,
+              :sort_by
 
-  def initialize(params)
-    @raw_info = params['docs']
+  def initialize(response_body, sort_params)
+    @raw_info = response_body['docs']
+    @sort_by  = sort_params[:sort_by]
+  end
+
+  def self.format_data(response_body, params)
+    books = new(response_body, params)
+    formatted_data = books.format_raw_info
+    books.sort(formatted_data)
   end
 
   def format_raw_info
