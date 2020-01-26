04a6831bdc1dffdbefb3cbea0c89dd1a452eb6a0
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 13:52:52 -0700

    calling presentation class for formatting raw info

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::SearchController < ApplicationController
   def index
     return invalid_search if search_params.empty?
+
     response = OpenLibraryService.get(search_params)
     is_search_unique = Search.is_search_unique?(search_params)
 
+    books_pres = BooksPresenter.new(JSON.parse(response.body))
+
     result = {
       is_search_unique: is_search_unique,
-      books: JSON.parse(response.body)
+      books: books_pres.format_raw_info
     }
 
     render json: result, status: 200
@@ -17,6 +20,7 @@ class Api::V1::SearchController < ApplicationController
   def search_params
     params.permit(:title, :author)
   end
+
 
   def invalid_search
     error = {
