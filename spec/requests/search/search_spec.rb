178aaf10e5db2d0bb0b28e751d6784f87a49a70f
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 12:16:59 -0700

    added test confirming 400 response if empty params are sent

--- spec/requests/search/search_spec.rb
+++ spec/requests/search/search_spec.rb
@@ -1,9 +1,17 @@
 RSpec.describe 'Search Controller', type: :request do
   context 'successful results' do
     it 'sends a valid response' do
+      # get '/api/v1/search'
+
+      # expect(response.code).to eq('200')
+    end
+  end
+
+  context 'unsuccessful requests' do
+    it 'sends a 400 if no search params are sent' do
       get '/api/v1/search'
 
-      expect(response.code).to eq('200')
+      expect(response.code).to eq('400')
     end
   end
 end
