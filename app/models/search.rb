d47e93a1d694daae38382a74f5a3ec14b1096b81
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 13:52:19 -0700

    format updates

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,11 +2,11 @@ class Search < ApplicationRecord
   belongs_to :title, optional: true
   belongs_to :author, optional: true
 
-  scope :find_title, lambda { |name| where(titles: { name: name }) }
+  scope :find_title,  lambda { |name| where(titles: { name: name })  }
   scope :find_author, lambda { |name| where(authors: { name: name }) }
 
   def self.is_search_unique?(params)
-    title =  params[:title]&.downcase
+    title  = params[:title]&.downcase
     author = params[:author]&.downcase
     is_search_empty = find_search(title, author).empty?
 
@@ -23,7 +23,7 @@ class Search < ApplicationRecord
   end
 
   def self.create_search(title_name, author_name)
-    title = Title.find_or_create_by(name: title_name)
+    title  = Title.find_or_create_by(name: title_name)
     author = Author.find_or_create_by(name: author_name)
 
     Search.create(title: title, author: author)
