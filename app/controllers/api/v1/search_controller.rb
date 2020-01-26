8c54023c7d0806cb3044aae22cea2dc661828c0e
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 11:31:03 -0700

    used Faraday to make connection to API, confirmed results rendered

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::SearchController < ApplicationController
   def index
-    result = {
-      data: 'sample data'
-    }
+    response = Faraday.get('http://openlibrary.org/search.json', { title: 'american+gods' })
+    result = JSON.parse(response.body)
+    # result = {
+    #   data: 'sample data'
+    # }
 
     render json: result, status: 200
   end
