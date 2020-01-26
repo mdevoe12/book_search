8243618601ef748cf5bb0176eb590d3a03ecda7c
Matt DeVoe <matthew.devoe@gmail.com>
2020-01-26 11:39:26 -0700

    added strong params for search, now able to pass params for query

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::SearchController < ApplicationController
   def index
-    # response = Faraday.get('http://openlibrary.org/search.json', { title: 'american+gods' })
-    response = OpenLibraryService.get
+    response = OpenLibraryService.get(search_params)
     result = JSON.parse(response.body)
-    # result = {
-    #   data: 'sample data'
-    # }
 
     render json: result, status: 200
+  end
+
+  private
+
+  def search_params
+    params.permit(:title)
   end
 end
